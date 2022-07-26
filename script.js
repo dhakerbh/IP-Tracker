9419590b92ccddf49ab2613e4ad4079c7ae7d027
Dhaker ben hamad <dhaker.geek@gmail.com>
2022-07-26 10:21:39 +0100

    1.1.1

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
-function getIPFromAmazon() {
-  fetch("https://checkip.amazonaws.com/")
-    .then((res) => res.text())
-    .then((data) => console.log(data));
-}
-
-const ip = getIPFromAmazon();
 const request = new XMLHttpRequest();
-console.log(ip);
-//request.open("GET", `http://ip-api.com/json/${ip}`);
+request.open("GET", "http://ip-api.com/json/");
+request.send();
+request.responseType = "json";
+request.onload = () => {
+  res = request.response;
+  var ip = res["query"];
+  var city = res["city"];
+  var code = res["countryCode"];
+  var zip = res["zip"];
+  var time = res["timezone"];
+  var isp = res["org"];
+  var lon = res["lon"];
+  var lat = res["lat"];
+
+  var loc = `${city}, ${code} ${zip}`;
+  document.getElementById("ip-adress").innerHTML = ip;
+  document.getElementById("loc").innerHTML = loc;
+  document.getElementById("time").innerHTML = time;
+  document.getElementById("isp").innerHTML = isp;
+  document
+    .getElementById("map")
+    .setAttribute(
+      "src",
+      `https://www.google.com/maps/embed/v1/place?key=AIzaSyA--xjTv9gFM5AqwOu4FvZfEsPwz58977Q&q=${lat},${lon}&zoom=6`
+    );
+};
+
+function GetIP() {
+  var insertedIP = document.getElementById("txt1").value;
+
+  const request = new XMLHttpRequest();
+  request.open("GET", `http://ip-api.com/json/${insertedIP}`);
+  request.send();
+  request.responseType = "json";
+  request.onload = () => {
+    res = request.response;
+    var ip = res["query"];
+    var city = res["city"];
+    var code = res["countryCode"];
+    var zip = res["zip"];
+    var time = res["timezone"];
+    var isp = res["org"];
+    var lon = res["lon"];
+    var lat = res["lat"];
+
+    var loc = `${city}, ${code} ${zip}`;
+    document.getElementById("ip-adress").innerHTML = ip;
+    document.getElementById("loc").innerHTML = loc;
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("isp").innerHTML = isp;
+    document
+      .getElementById("map")
+      .setAttribute(
+        "src",
+        `https://www.google.com/maps/embed/v1/place?key=AIzaSyA--xjTv9gFM5AqwOu4FvZfEsPwz58977Q&q=${lat},${lon}&zoom=6`
+      );
+  };
+}
